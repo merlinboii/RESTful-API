9727dd88d3cd14144b4444978794f09ff768517a
filmptz <parichaya35@gmail.com>
2020-12-22 12:06:45 +0700

    Modified
    Change port to 4000
    Delete DemoApplication.java

---
+++ demo/src/test/java/org/apiproject/boot/demo/restAPITest.java
@@ -0,0 +1,13 @@
+package org.apiproject.boot.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootTest
+@RestController
+@RequestMapping("/api")
+
+public class restAPITest {
+    
+}
