c5b780bea95f1978ab54d7d5a4d3cb7768bb1ac3
filmptz <parichaya35@gmail.com>
2020-12-22 12:01:54 +0700

    Change name and Constructor

--- demo/src/main/java/org/apiproject/boot/demo/Education.java
+++ demo/src/main/java/org/apiproject/boot/demo/Education.java
@@ -2,25 +2,34 @@ package org.apiproject.boot.demo;
 
 public class Education {
     private String degree;
-    private UniversityInfo uInfo ;
+    private String uName;
 
-     public Education(String degree, UniversityInfo uInfo) {
-        this.degree=degree;
-        this.uInfo=uInfo;
-     }
-     public void setDegree(String degree){
+    public Education(String degree, String uName) {
+        this.degree = degree;
+        this.setuName(uName);
+    }
+
+    public String getuName() {
+        return uName;
+    }
+
+    public void setuName(String uName) {
+        this.uName = uName;
+    }
+
+    public void setDegree(String degree) {
         this.degree=degree;
     }
     public String getDegree(){
         return degree;
     }
 
-    public UniversityInfo getUInfo() {
+    /* public UniversityInfo getUInfo() {
         return uInfo;
     }
 
     public void setUInfo(UniversityInfo uInfo) {
         this.uInfo = uInfo;
-    }  
+    }   */
 
 }
--- demo/src/main/java/org/apiproject/boot/demo/UniversityInfo.java
+++ demo/src/main/java/org/apiproject/boot/demo/UniversityInfo.java
@@ -1,14 +1,19 @@
 package org.apiproject.boot.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UniversityInfo {
-    private long id ;
+    private long id;
     private String name;
     private String name_init;
-    
-    public UniversityInfo(long id,String name,String name_init){
+    private List<String> name_std = new ArrayList<>();
+
+    public UniversityInfo(long id, String name, String name_init) {
         this.id = id;
         this.name = name;
         this.name_init = name_init;
+        //this.name_std = name_std;
     }
 
     public void setId(long id){
@@ -31,4 +36,17 @@ public class UniversityInfo {
     public void setName_init(String name_init) {
         this.name_init = name_init;
     }
+
+    public List<String> getName_std() {
+        return name_std;
+    }
+
+    public void setName_std(List<String> name_std) {
+        this.name_std = name_std;
+    }
+
+    public void addName_std(String name){
+        name_std.add(name);
+    }
+
 }
